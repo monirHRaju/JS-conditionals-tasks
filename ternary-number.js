eea65443e9690980ba2417a4d5d45abd65d205a4
monirHRaju <monirhraju@gmail.com>
2025-07-30 05:52:52 +0600

    termary number calculation done

--- ternary-number.js
+++ ternary-number.js
@@ -10,3 +10,18 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+let num1 = 5; // Example value
+let num2 = 10; // Example value
+let result;
+// // Using if-else
+// if (num1 > num2) {
+//     result = num1 * 2;
+// } else {
+//     result = num1 + num2;
+// }
+// console.log("Result using if-else:", result);
+
+// Using ternary operator
+
+result = num1 > num2 ? num1 * 2 : num1 + num2;
+console.log("Result using ternary operator:", result);
